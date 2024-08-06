5eb14ac000452dd83ef658e4479d02b5a9a9b9cd
Arnel Francia <nelffff@gmail.com>
2024-08-06 20:31:04 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import AddSave from "./components/AddSave.jsx";
+
 import ContentMain from "./components/ContentMain.jsx";
 import Hero from "./components/Hero.jsx";
 import Social from "./components/Social.jsx";
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <Hero />
-      <AddSave />
       <Social />
       <ContentMain />
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,42 @@ import imagePath from "../images/hero.jpg";
 
 export default function Hero() {
   return (
-    <div className="mx-auto my-8 w-96 sm:w-custom-width-2 md:w-custom-width-1 md:mx-20 sm:mx-12 justify-center">
-      <img
-        className="rounded-2xl w-72 object-bottom mx-12 md:w-full sm:w-full"
-        src="https://c4.wallpaperflare.com/wallpaper/429/742/368/breaking-bad-rv-wallpaper-preview.jpg"
-      />
-      <div className="w-24 md:w-32 md:h-32 h-24 mx-auto -mt-12 md:mx-60 sm:mx-56">
+    <div className="flex flex-col justify-center">
+      <div className="mx-auto my-8 w-96 sm:w-custom-width-2 md:w-custom-width-1  ">
         <img
-          src={imagePath}
-          className="w-full h-full rounded-full object-cover"
+          className="rounded-2xl w-72 object-bottom mx-12 md:w-full sm:w-full"
+          src="https://c4.wallpaperflare.com/wallpaper/429/742/368/breaking-bad-rv-wallpaper-preview.jpg"
         />
+        <div className="w-24 md:w-32 md:h-32 h-24 mx-auto -mt-12 md:mx-60 sm:mx-56">
+          <img
+            src={imagePath}
+            className="w-full h-full rounded-full object-cover"
+          />
+        </div>
+
+        <div className="w-full  grid justify-center text-center my-3 font-thin md:mx-12 sm:mx-10">
+          <h2 className="font-bold text-3xl">Arnel Francia</h2>
+          <p className="font-semibold">Codebility Trainee</p>
+          <p>Nelffff@gmail.com</p>
+          <p>+639065412970</p>
+        </div>
       </div>
 
-      <div className="w-full  grid justify-center text-center my-3 font-thin md:mx-12 sm:mx-10">
-        <h2 className="font-bold text-3xl">Arnel Francia</h2>
-        <p className="font-semibold">Codebility Trainee</p>
-        <p>Nelffff@gmail.com</p>
-        <p>+639065412970</p>
+      <div className="flex justify-center -m-4">
+        <div className="w-12 h-12 mr-3">
+          <img
+            className="rounded-full border border-black overflow-hidden w-full h-full"
+            src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-vector-envelope-icon-png-image_695356.jpg"
+          />
+          <p className="text-center text-sm font-semibold">Add</p>
+        </div>
+        <div className="w-12 h-12">
+          <img
+            className="rounded-full border border-black overflow-hidden w-full h-full object-scale-down"
+            src="https://w7.pngwing.com/pngs/942/419/png-transparent-bookmark-favorite-love-save-like-essential-icon-thumbnail.png"
+          />
+          <p className="text-center text-sm font-semibold">Save</p>
+        </div>
       </div>
     </div>
   );
