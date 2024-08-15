47df1538c917c32bc33abf6e11e781d41a9b292a
Arnel Francia <nelffff@gmail.com>
2024-08-15 20:08:50 +0800

    updated

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,36 +1,42 @@
 export default function Social() {
   return (
     <div className="my-10 flex justify-center mr-4  lg:justify-start lg:ml-52 lg:-mt-16">
-      <div className="w-12 h-12 lg:w-16 lg:h-16"></div>
-      <div className="w-12 h-12 lg:w-16 lg:h-16">
+      <div className="w-12"></div>
+
+      <div>
         <img
-          className="rounded-full overflow-hidden w-full h-full object-scale-down"
+          className="w-16 rounded-full object-center"
           src="https://i.pinimg.com/474x/43/bb/91/43bb91aa0bd9a847f1cd9bbb413ab980.jpg"
-        />
+        />{" "}
+        {/*FB logo*/}
       </div>
-      <div className="w-12 h-12 mt-2 ml-3">
+      <div>
         <img
-          className="rounded-full overflow-hidden w-8 h-8 lg:w-14 lg:h-14 object-center"
+          className="w-16 rounded-full"
           src="https://t4.ftcdn.net/jpg/06/28/20/77/360_F_628207702_IXEoMmDH6PQQMysRLxqmyMRZomT8qvTc.jpg"
         />
+        {/*X logo*/}
       </div>
-      <div className="w-12 h-12 mr-3 lg:w-16 lg:h-16">
+      <div>
         <img
-          className="rounded-full overflow-hidden w-full h-full object-scale-down"
+          className="w-20 rounded-full"
           src="https://i.pinimg.com/originals/f0/70/3e/f0703e34b09159d23b2a6b3abcbcecb8.jpg"
         />
+        {/*messenger logo*/}
       </div>
-      <div className="w-12 h-12 mt-2 lg:mr-3 ">
+      <div>
         <img
-          className="rounded-full overflow-hidden w-7 h-7 lg:w-11 lg:h-11 object-center"
+          className="w-12 rounded-full"
           src="https://i.pinimg.com/474x/3a/e3/8f/3ae38f3d9944404e556a6db3ed91932b.jpg"
         />
+        {/*globe logo*/}
       </div>
-      <div className="w-12 h-12 mt-1">
+      <div className="pl-4">
         <img
-          className="rounded-full overflow-hidden object-scale-down w-8 h-8 lg:w-11 lg:h-11"
+          className="w-12 rounded-full"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLGwEKQyKgraBY4eJDjWAsFkU3h_6GXTJnOw&s"
         />
+        {/*phone logo*/}
       </div>
     </div>
   );
